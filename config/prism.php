371217364f61ab697170fc77b8c39b68671799ac
rofiuddin15 <kambing.rofi@gmail.com>
2026-06-25 21:20:48 +0700

    update ai

--- config/prism.php
+++ config/prism.php
@@ -39,7 +39,7 @@ return [
         ],
         'gemini' => [
             'api_key' => env('GEMINI_API_KEY', ''),
-            'url' => env('GEMINI_URL', 'https://generativelanguage.googleapis.com/v1/models'),
+            'url' => env('GEMINI_URL', 'https://generativelanguage.googleapis.com/v1beta/models'),
         ],
         'deepseek' => [
             'api_key' => env('DEEPSEEK_API_KEY', ''),
